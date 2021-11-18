f091d469ce0bf55a20e21e158141480ea371c6f0
yuto90 <solidusgako4087@gmail.com>
2021-11-18 17:35:28 +0900

    mypage画面作成

---
+++ lib/mypage/mypage.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:kinender_mobile/header/header.dart';
+import 'package:provider/provider.dart';
+import 'mypage_model.dart';
+
+class Mypage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('mypage'));
+  }
+}
