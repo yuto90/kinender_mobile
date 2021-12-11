db4fec54d6e27353efdc34dae07769cfe27fdaf3
yuto90 <solidusgako4087@gmail.com>
2021-12-12 01:35:56 +0900

    新しく払い出されたトークンを保存する用の関数を作成

--- lib/helper.dart
+++ lib/helper.dart
@@ -1,2 +1,22 @@
 // helper関数を置くファイル
-class Helper {}
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Helper {
+  // TokenRefreshAPIから戻された新しいトークンをローカルストレージに保存する
+  static Future setNewToken(http.Response token) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    String convertNewToken = utf8.decode(token.bodyBytes);
+    dynamic newToken = jsonDecode(convertNewToken);
+
+    String newAccessToken = newToken['access'];
+    String newRefreshToken = newToken['refresh'];
+    newAccessToken = 'JWT ' + newAccessToken;
+    newRefreshToken = 'JWT ' + newRefreshToken;
+
+    prefs.setString('accessToken', newAccessToken);
+    prefs.setString('refreshToken', newRefreshToken);
+  }
+}
